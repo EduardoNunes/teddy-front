54ca7ab738821dcb47a429dc6b2fe41e80e89e08
Eduardo <eduardolagonunes@gmail.com>
2024-12-05 12:57:01 -0300

    FEAT: criado layout do modal delete

---
+++ src/components/ModalDelete.tsx
@@ -0,0 +1,32 @@
+import Button from "./Button";
+
+export default function ModalDelete() {
+  return (
+    <div className="absolute top-0 left-0 flex justify-center items-center w-[100vw] h-[100vh] bg-colorbgmodal z-10">
+      <div className="flex flex-col justify-center items-center gap-2 w-[400px] h-[148px] bg-fullwhite px-[20px] rounded-[4px]">
+        <div className="flex justify-between items-center w-full">
+          <h1 className="font-inter font-bold text-size16 text-textblack">
+            Excluir cliente:
+          </h1>
+          <button>
+            <img
+              src="/public/images/exit.svg"
+              alt="Ícone de sair"
+              className="w-3 h-3"
+            />
+          </button>
+        </div>
+        <h2 className="w-full text-start">
+          Você está prestes a excluir o cliente: <strong>Eduardo</strong>
+        </h2>
+        <div className="w-full mt-2">
+          <Button
+            text="Criar cliente"
+            style={{ height: "40px", fontSize: "14px" }}
+          />
+        </div>
+      </div>
+      ;
+    </div>
+  );
+}

f1b24dc199f7b1633d98cb619e8f7173c1c11a0b
Eduardo <eduardolagonunes@gmail.com>
2024-12-05 11:46:32 -0300

    FEAT: layout do header estruturado.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,39 +4,49 @@ export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <header className="w-full bg-white shadow-md px-6 py-4">
-      <div className="flex items-center justify-between">
-        <div className="flex items-center space-x-2">
-          <button onClick={() => setIsMenuOpen(!isMenuOpen)}>
-            <img
-              src="/public/images/hamburger.svg"
-              alt="Logo"
-              className="w-6 h-5"
-            />
-          </button>
-          <img
-            src="/public/images/logo.svg"
-            alt="Logo"
-            className="w-[100px] h-[48.98px]"
-          />
-        </div>
-
-        <div className="hidden lg:flex space-x-8">
-          <a href="#" className="text-sm text-gray-700 hover:text-orange-500">
+    <header className="relative flex justify-center w-full bg-white shadow-md px-14 py-4">
+      <button
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className="absolute left-16 top-[30px]"
+      >
+        <img
+          src="/public/images/hamburger.svg"
+          alt="Logo"
+          className="w-6 h-5"
+        />
+      </button>
+      <div className="flex items-center justify-between w-[85%]">
+        <img
+          src="/public/images/logo.svg"
+          alt="Logo"
+          className="w-[100px] h-[48.98px]"
+        />
+        <div className="space-x-12">
+          <a
+            href="#"
+            className="font-inter font-normal text-size16 text-textblack hover:text-colororange hover:underline"
+          >
             Clientes
           </a>
-          <a href="#" className="text-sm text-gray-700 hover:text-orange-500">
+          <a
+            href="#"
+            className="font-inter font-normal text-size16 text-textblack hover:text-colororange hover:underline"
+          >
             Clientes selecionados
           </a>
-          <a href="#" className="text-sm text-gray-700 hover:text-orange-500">
+          <a
+            href="#"
+            className="font-inter font-normal text-size16 text-textblack hover:text-colororange hover:underline"
+          >
             Sair
           </a>
         </div>
-
-        <div className="text-sm text-gray-700">
-          <span>Olá, Usuário!</span>
+        <div className="font-inter font-normal text-size16 text-textblack">
+          <span>
+            Olá, <strong>Usuário!</strong>
+          </span>
         </div>
-      </div>     
+      </div>
     </header>
   );
 }

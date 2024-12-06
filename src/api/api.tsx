104be446a20be0a43e2f8a8e55cdf8f7f50e9042
Eduardo <eduardolagonunes@gmail.com>
2024-12-06 15:04:12 -0300

    DEPLOY

--- src/api/api.tsx
+++ src/api/api.tsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://teddy-back.onrender.com",
   timeout: 100000,
   headers: { "Content-Type": "application/json" },
 });
 
 export default api;
+//http://localhost:3000

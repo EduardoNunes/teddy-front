b52c0deab536f331d650a53861a89621d5660dfe
Eduardo <eduardolagonunes@gmail.com>
2024-12-06 07:02:58 -0300

    FEAT: adicionado navegação e função de sair da aplicação

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,21 @@ import { useEffect, useState } from "react";
 import Cookies from "js-cookie";
 import SideBar from "./SideBar";
 import { useGlobalContext } from "../context/globalContext";
+import { useLocation } from "react-router-dom";
 
 export default function Header() {
   const { isOpenMenu, setIsOpenMenu } = useGlobalContext();
   const [userName, setUserName] = useState("");
+  const pathName = useLocation();
 
   useEffect(() => {
     const user = Cookies.get("userName");
     setUserName(user || "");
   }, []);
+
+  const handleExit = () => {
+    Cookies.remove("userName");
+  };
 
   return (
     <header className="relative flex justify-center w-full bg-white shadow-md py-4">
@@ -29,27 +35,36 @@ export default function Header() {
         />
         <div className="space-x-12">
           <a
-            href="#"
-            className="font-inter font-normal text-size16 text-textblack hover:text-colororange hover:underline"
+            href="/clients"
+            className={`font-inter font-normal text-size16 ${
+              pathName.pathname === "/clients"
+                ? "text-colororange underline"
+                : "text-textblack"
+            }`}
           >
             Clientes
           </a>
           <a
-            href="#"
-            className="font-inter font-normal text-size16 text-textblack hover:text-colororange hover:underline"
+            href="/selected-clients"
+            className={`font-inter font-normal text-size16 ${
+              pathName.pathname === "/selected-clients"
+                ? "text-colororange underline"
+                : "text-textblack"
+            }`}
           >
             Clientes selecionados
           </a>
           <a
-            href="#"
-            className="font-inter font-normal text-size16 text-textblack hover:text-colororange hover:underline"
+            href="/"
+            className="font-inter font-normal text-size16 text-textblack "
+            onClick={handleExit}
           >
             Sair
           </a>
         </div>
         <div className="font-inter font-normal text-size16 text-textblack">
           <span>
-            Olá, <strong>{userName}!</strong>
+            Olá, <strong>{userName.split(" ")[0]}!</strong>
           </span>
         </div>
       </div>
